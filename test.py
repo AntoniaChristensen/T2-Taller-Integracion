f3cefb2abb43de8936a0517e01d7271b4149d625
AntoniaChristensen <achristensen1@uc.cl>
2021-04-23 16:43:58 -0400

    no requests

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import requests
+#import requests
 
-BASE = "http://127.0.0.1:5000/"
+#BASE = "http://127.0.0.1:5000/"
    
 
-response = requests.delete(BASE + "artists/TWljaGFlbCBKYWNrc29u")
-print(response.json())
-input()
-response = requests.get(BASE + "tracks")
-print(response.json())
+#response = requests.delete(BASE + "artists/TWljaGFlbCBKYWNrc29u")
+#print(response.json())
+#input()
+#response = requests.get(BASE + "tracks")
+#print(response.json())
